6422e0d17b6c3c9a4a7c7fdb5090f31f14231a68
novemberluch <50756298+novemberluch@users.noreply.github.com>
2022-03-24 15:18:20 -0400

    Add file_paths.xml
    Add file_paths.xml, update AndroidManifest.xml and MainActivity.kt

--- app/src/main/java/com/example/pupbuddym/MainActivity.kt
+++ app/src/main/java/com/example/pupbuddym/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.pupbuddym
 
+import android.Manifest
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.pupbuddym.ui.theme.PupBuddyMTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,20 +29,52 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+                Button(
+                    onClick = {
+                        takePhoto()
+                    }
+                ) {
+                    Text(text = "Photo")
+                }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun takePhoto() {
+        if (hasCameraPermission() == PERMISSION_GRANTED && hasExternalStoragePermission() == PERMISSION_GRANTED) {
+            invokeCamera()
+        } else {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PupBuddyMTheme {
-        Greeting("Android")
+        }
+    }
+
+    private val requestMultiplePermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        resultsMap ->
+        var permissionsGranted = false
+        resultsMap.forEach {
+            if (it.value == true) {
+
+            }
+        }
+    }
+
+    private fun invokeCamera() {
+        TODO("Not yet implemented")
+    }
+
+    fun hasCameraPermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    fun hasExternalStoragePermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        PupBuddyMTheme {
+            Greeting("Android")
+        }
     }
 }
