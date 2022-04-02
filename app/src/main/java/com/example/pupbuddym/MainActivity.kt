3f16ad33a1d472888a3e1389cc5669553a5170e6
Yep-Was-Taken <herreray@mail.uc.edu>
2022-04-01 20:35:00 -0400

    pushing working GPS feature to branch

--- app/src/main/java/com/example/pupbuddym/MainActivity.kt
+++ app/src/main/java/com/example/pupbuddym/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.pupbuddym
 
 import android.Manifest
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
@@ -39,7 +44,11 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), LocationListener {
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
 
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
@@ -47,7 +56,6 @@ class MainActivity : ComponentActivity() {
     private var currentImagePath: String = ""
     private var strUri by mutableStateOf("")
     val viewModel by viewModels<MainViewModel>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,38 +65,34 @@ class MainActivity : ComponentActivity() {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        findViewById<Button>(R.id.btn_get_location).setOnClickListener {
-            fetchLocation()
+        val button: Button = findViewById(R.id.getLocation)
+        button.setOnClickListener {
+            getLocation()
         }
     }
 
-    private fun fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                101
-            )
-            return
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
         }
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            if (it != null) {
-                Toast.makeText(
-                    applicationContext,
-                    "${it.latitude} ${it.longitude}",
-                    Toast.LENGTH_SHORT
-                ).show()
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        tvGpsLocation = findViewById(R.id.textView)
+        tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 
     private fun signIn() {
